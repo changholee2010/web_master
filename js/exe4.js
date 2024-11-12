9158d352ce930cb6f1a9d8517a638fb094811ab6
pc <pc@email.com>
2024-11-12 18:16:42 +0900

    d

--- js/exe4.js
+++ js/exe4.js
@@ -5,4 +5,30 @@
 // 배열, 반복문, 조건문 활용.
 // 우리반 평균은 ??이고, 최고점은 ??이고, 최저점은 ??이다.
 
-console.log('exe4.js');
+let scoreAry = [];
+for (let i = 1; i <= 5; i++) {
+  scoreAry.push(prompt('점수를 입력'));
+}
+
+let sum = 0;
+let max = 0,
+  min = scoreAry[0];
+let average = 0;
+// 정렬.
+let temp = 0;
+for (let i = 0; i < scoreAry.length - 1; i++) {
+  for (let j = 0; j < scoreAry.length - 1; j++) {
+    if (scoreAry[i] > scoreAry[i + 1]) { // 정렬.
+      temp = scoreAry[i];
+      scoreAry[i] = scoreAry[i + 1];
+    }
+  }
+}
+
+sum = scoreAry.reduce((accm, item) => {
+  return accm + item;
+}, 0);
+average = sum / scoreAry.length;
+max = scoreAry[4];
+min = scoreAry[0];
+console.log(`우리반 평균은 ${average}이고, 최고점은 ${max}이고, 최저점은 ${min}이다.`);
