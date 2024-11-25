47df80f07d5b324fc7016fea81bfb5be359b4600
pc <pc@email.com>
2024-11-25 15:29:04 +0900

    java

--- HelloJava/src/com/yedam/HelloJava.java
+++ HelloJava/src/com/yedam/HelloJava.java
@@ -1,8 +1,8 @@
 package com.yedam;
 
 public class HelloJava {
-	private void sysout() {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {
 		System.out.println("Hello");
+
 	}
 }
